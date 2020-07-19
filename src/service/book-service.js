42491b3672820ad1fb087f236f2b43578f25a951
Gledson A <g_a6@live.com>
2020-07-19 15:07:15 -0300

    Adding method to generate book's URLs file

--- src/service/book-service.js
+++ src/service/book-service.js
@@ -1,7 +1,44 @@
+const fs = require('fs');
+
+const environment = require('../configuration/environment');
 const { get } = require('./http-service');
 const { getTitleFrom, getEntry, getNextPageUrlFrom, getConfigurationFor } = require('../util/book-utils');
 
-const getPageFor = async ({currentPageUrl, lastPageUrl, typos}) => {
+const BOOK_URLS_DIRECTORY = 'src/configuration/book-urls';
+
+const _getBookUrlsFor = async (currentPageUrl, lastPageUrl, urls = [currentPageUrl]) => {
+    const webpage = await get(currentPageUrl);
+    const url = (`${currentPageUrl}` !== `${lastPageUrl}`) ? getNextPageUrlFrom(webpage) : undefined;
+
+    if (url) {
+        return _getBookUrlsFor(url, lastPageUrl, [...urls, url]);
+    } else {
+        return urls;
+    }
+};
+
+const generateBookUrlsFileFor = async (book) => {
+    const { firstPageUrl, lastPageUrl } = environment[book];
+
+    let template = '[\nurls\n]';
+    let urlsString = '';
+
+    const urls = await _getBookUrlsFor(firstPageUrl, lastPageUrl);
+    urls.forEach(url => {
+        urlsString += `"${url}",\n`;
+    });
+
+    urlsString = template.replace('urls', urlsString);
+    urlsString = urlsString.replace(',\n\n]', '\n]');
+
+    if (!fs.existsSync(BOOK_URLS_DIRECTORY)) {
+        fs.mkdirSync(BOOK_URLS_DIRECTORY);
+    }
+
+    fs.writeFileSync(`${BOOK_URLS_DIRECTORY}/${book}_urls.json`, urlsString);
+};
+
+const getPageFor = async ({ currentPageUrl, lastPageUrl, typos }) => {
     const webpage = await get(currentPageUrl);
 
     const title = getTitleFrom(webpage);
@@ -14,15 +51,16 @@ const getPageFor = async ({currentPageUrl, lastPageUrl, typos}) => {
 const getPagesFor = async (book, pages = []) => {
     const configuration = getConfigurationFor(book);
 
-    const page = await getPageFor(configuration); // need to be parallel. It will take too long otherwise
+    // need to be parallel/concurrent. It will take too long otherwise. Try this: https://itnext.io/node-js-handling-asynchronous-operations-in-parallel-69679dfae3fc
+    const page = getPageFor(configuration);
     pages.push(page);
 
     if (page.nextPageUrl) {
         configuration.currentPageUrl = page.nextPageUrl;
-        return await getPagesFor(book, pages);
+        return getPagesFor(book, pages);
     } else {
         return pages;
     }
 };
 
-module.exports = { getPageFor, getPagesFor };
+module.exports = { generateBookUrlsFileFor, getPageFor, getPagesFor };
